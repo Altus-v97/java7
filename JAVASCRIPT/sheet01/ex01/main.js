a0e62ffe5dafc160ddd4d825b4203e25b4921a0d
Marou <m.arrihani8@gmail.com>
2025-10-14 12:07:30 +0200

    feat: Publishing ex01

---
+++ JAVASCRIPT/sheet01/ex01/main.js
@@ -0,0 +1,12 @@
+var name = "Marou" ; 
+let age = 28; 
+const cityMalaga = true;
+
+function print() {
+    console.log(`Hello, ${name}!`)
+    console.log (`Lives in Malaga: ${cityMalaga}`)
+    console.log (`Age: ${age}`)
+}
+
+
+print();
