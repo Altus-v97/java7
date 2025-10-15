e10dd85ac2b9a0960b1ade811a334abe8a9f141e
Marou <m.arrihani8@gmail.com>
2025-10-15 13:38:12 +0200

    feat: ex13

---
+++ JAVASCRIPT/sheet01/ex13/main.js
@@ -0,0 +1,15 @@
+var globalA = 1000;
+console.log(globalA);
+
+function local(){
+    let localA = 100;
+    var localB = 50;
+    console.log(localA);
+    console.log(localB);
+    console.log(globalA);
+
+}
+local()
+console.log(globalA) // 1000
+console.log(localA) // undefined
+console.log(localB) // undefined
