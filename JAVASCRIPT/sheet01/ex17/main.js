5116d33a4f71ed37c7ab99a22f1342bdff907762
Marou <m.arrihani8@gmail.com>
2025-10-18 22:14:11 +0200

    publishing sheet03 ex01-04

---
+++ JAVASCRIPT/sheet01/ex17/main.js
@@ -0,0 +1,2 @@
+console.log(this === window);
+console.log(globalThis);
--- JAVASCRIPT/sheet03/ex04/main.js
+++ JAVASCRIPT/sheet03/ex04/main.js
@@ -5,10 +5,12 @@ const users = [
 ];
 
 function mayoresMayusculos(arr) {
-    const mayores = arr.filter(users => users.age > 18);
+    const mayores = arr.filter(users => users.age >= 18);
     const result = mayores.map(users => ({...users, name: users.name.toUpperCase()}));
     return result;
 }
 
 const resultado = mayoresMayusculos(users);
 console.log(resultado);
+
+
